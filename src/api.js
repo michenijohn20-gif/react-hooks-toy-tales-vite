6b19844ce273391229ad1128a513f42dbe68a423
John Muthomi <michenijohn20@gmail.com>
2026-05-12 16:09:00 +0300

    Wire toy state and API loading

---
+++ src/api.js
@@ -0,0 +1,1 @@
+export const TOYS_URL = "http://localhost:3001/toys";
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
+import { TOYS_URL } from "../api";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch(TOYS_URL)
+      .then((response) => response.json())
+      .then((toys) => setToys(toys));
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleAddToy(newToy) {
+    setToys((toys) => [...toys, newToy]);
+  }
+
+  function handleLikeToy(updatedToy) {
+    setToys((toys) =>
+      toys.map((toy) => (toy.id === updatedToy.id ? updatedToy : toy))
+    );
+  }
+
+  function handleDeleteToy(deletedToy) {
+    setToys((toys) => toys.filter((toy) => toy.id !== deletedToy.id));
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy={handleAddToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer
+        toys={toys}
+        onLikeToy={handleLikeToy}
+        onDeleteToy={handleDeleteToy}
+      />
     </>
   );
 }
